c1e3a42a9b7d8d523545757e5c6fb49528835fbb
wishba <wisnubayu34@gmail.com>
2024-11-23 21:04:25 +0700

    fix cors origin

--- src/app.js
+++ src/app.js
@@ -2,28 +2,27 @@ const Hapi = require('@hapi/hapi');
 const { loadModel, predict } = require('./inference');
 
 (async () => {
-  // load and get machine learning model
   const model = await loadModel();
   console.log('model loaded!');
 
-  // initializing HTTP server
   const server = Hapi.server({
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    port: 3000
+    port: 3000,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
   });
 
   server.route({
     method: 'POST',
     path: '/predict',
     handler: async (request) => {
-      // get image that uploaded by user
       const { image } = request.payload;
-      // do and get prediction result by giving model and image
       const predictions = await predict(model, image);
-      // get prediction result
       return { result: predictions };
     },
-    // make request payload as `multipart/form-data` to accept file upload
     options: {
       payload: {
         allow: 'multipart/form-data',
@@ -32,7 +31,6 @@ const { loadModel, predict } = require('./inference');
     }
   });
 
-  // running server
   await server.start();
 
   console.log(`Server start at: ${server.info.uri}`);
